4fcdde8868a55dcca59f0994a7a4d8da5b3610fc
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2025-07-29 22:00:52 -0700

    added css

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,18 @@
 export default function Home(){
-  return <div>
-    <div>
-      
+  return (
+    <div className="container">
+      <div className="content">
+        <div className="border">
+          <div className="month">
+            <p>Month</p>
+          </div>
+          <div className="others">
+            <p className="param">Day</p>
+            <p className="number">2</p>
+            <p className="param">year</p>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
+  );
 }
