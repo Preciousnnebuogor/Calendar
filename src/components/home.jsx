95a2f4a72e2d53098eaff944142208d0311a51dc
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2025-07-29 22:38:58 -0700

    collected

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,15 +1,22 @@
+
+
 export default function Home(){
+  const date = new Date()
+  const resultMonth = date.toLocaleString("en", { month: "long" });
+  const day = date.getDate()
+  const resultDay = date.toLocaleString("en", { weekday: "long" });
+  const year = date.getFullYear()
   return (
     <div className="container">
       <div className="content">
         <div className="border">
           <div className="month">
-            <p>Month</p>
+            <p>{resultMonth}</p>
           </div>
           <div className="others">
-            <p className="param">Day</p>
-            <p className="number">2</p>
-            <p className="param">year</p>
+            <p className="param">{resultDay}</p>
+            <p className="number">{day}</p>
+            <p className="param">{year}</p>
           </div>
         </div>
       </div>
